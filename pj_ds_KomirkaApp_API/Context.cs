3b706f990fb690b3489d37c548274dad3d9b76b1
YaroslavHerhil <yarik.342.ua.itstep@gmail.com>
2025-05-26 14:03:15 +0200

    Add project files.

---
+++ pj_ds_KomirkaApp_API/Context.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.Extensions.Hosting;
+using pj_ds_KomirkaApp_API.Models;
+using System;
+using System.Reflection.Metadata;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using ConfigurationManager = System.Configuration.ConfigurationManager;
+
+namespace pj_ds_KomirkaApp_API
+{
+    public class Context : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+
+        // Temp
+        private string _connectionString = "Data Source = (localdb)\\MSSQLLocalDB;Initial Catalog = KomirkaDb; Integrated Security = True; Connect Timeout = 30;";
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(_connectionString);
+        }
+    }
+}
---
+++ pj_ds_KomirkaApp_API/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace pj_ds_KomirkaApp_API.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
